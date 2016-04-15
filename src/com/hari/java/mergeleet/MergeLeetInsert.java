583d5e7178144b943e2a51d9df228387e1ac71c9
root <tech4appa@gmail.com>
2016-04-15 08:30:13 -0700

    Implementation of Leet Code with Insertion

--- src/com/hari/java/mergeleet/MergeLeetInsert.java
+++ src/com/hari/java/mergeleet/MergeLeetInsert.java
@@ -8,15 +8,17 @@ import java.util.StringTokenizer;
 import com.hari.utils.IOUtils;
 
 /**
- * Given a collection of intervals, merge all overlapping intervals.
- * For example,
- * Given [1,3],[2,6],[8,10],[15,18],
- * return [1,6],[8,10],[15,18].
+ * Example 1:
+ * Given intervals [1,3],[6,9], insert and merge [2,5] in as [1,5],[6,9].
+ * Example 2:
+ * Given [1,2],[3,5],[6,7],[8,10],[12,16], insert and merge [4,9] in as [1,2],[3,10],[12,16].
+ * This is because the new interval [4,9] overlaps with [3,5],[6,7],[8,10].
+ * 
  * @author venkat
  *
  */
 
-public class MergeLeet {
+public class MergeLeetInsert {
 
 	public static void main(String[] args) {
 		System.out.print("Enter inputs in the form of a,b|c,d : ");
@@ -28,30 +30,31 @@ public class MergeLeet {
 			intervals.add(new Interval(Integer.parseInt(tokenArray[0]), Integer.parseInt(tokenArray[1])));
 		}
 		
+		System.out.print("Enter leet to be inserted a,b : ");
+		String insertStr = IOUtils.readString();
+		String[] insertArray = insertStr.split(",");
+		Interval insertInterval = new Interval(Integer.parseInt(insertArray[0]), Integer.parseInt(insertArray[1]));
+		
+		intervals.add(insertInterval);
+		
+		
 		Collections.sort(intervals, new IntervalComparator());
-
-		display(intervals);
+		MergeLeet.display(intervals);
 		
 		List<Interval> result = new ArrayList<Interval>();
 		Interval prev = intervals.get(0);
-		for(int ind=0; ind<intervals.size(); ind++) {
+		for(int ind=1; ind<intervals.size(); ind++) {
 			Interval curr = intervals.get(ind);
-			if (prev.getEnd() < curr.getStart()) {
+			if (prev.getEnd() > curr.getStart()) {
+				prev = new Interval(prev.getStart(), Math.max(curr.getEnd(), prev.getEnd()));
+			} else {
 				result.add(prev);
 				prev = curr;
-			} else {
-				prev = new Interval(prev.getStart(), Math.max(curr.getEnd(), prev.getEnd()));
 			}
 		}
 		result.add(prev);
-
-		display(result);
-	}
-
-	public static void display(List<Interval> intervals) {
-		for(Interval interval : intervals) {
-			System.out.println(interval.toString());
-		}
+		
+		MergeLeet.display(result);
 	}
 	
 }
